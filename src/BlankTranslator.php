dd2a64bdb1e220f0ff89a2773f2a02909b22083e
Stanislav Vojíř <stanislav.vojir@gmail.com>
2017-10-26 18:05:49 +0200

    setLang, interface renamed

--- src/BlankTranslator.php
+++ src/BlankTranslator.php
@@ -8,7 +8,9 @@ namespace Vojir\BlankTranslator;
  * @author Stanislav Vojíř
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
-class BlankTranslator implements IMultilanguageTranslator{
+class BlankTranslator implements ITranslator{
+
+  private $lang = 'en';
 
   /**
    * Translates the given string.
@@ -25,7 +27,14 @@ class BlankTranslator implements IMultilanguageTranslator{
    * @return string
    */
   public function getLang() {
-    return "en";
+    return $this->lang;
   }
 
+  /**
+   * Method for selection of the actual language
+   * @param string $language
+   */
+  public function setLang($language){
+    $this->lang=$language;
+  }
 }
