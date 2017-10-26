e058df2e3e1d333c9880e6866554c84e5e040fcf
Stanislav Vojíř <stanislav.vojir@gmail.com>
2017-10-26 18:35:28 +0200

    extension config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Nette-BlankTranslator
 Simple, blank translator for Nette projects
+
+
+For usage, include into your config.neon:
+```
+    BlankTranslator: Vojir\BlankTranslator\DI\BlankTranslatorExtension
+```
---
+++ src/DI/BlankTranslatorExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Vojir\BlankTranslator\DI;
+
+/**
+ * Class BlankTranslatorExtension
+ * @package Vojir\BlankTranslator\DI
+ * @author Stanislav Vojíř
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+class BlankTranslatorExtension extends Nette\DI\CompilerExtension{
+
+  public function loadConfiguration(){
+    $container = $this->getContainerBuilder();
+    $container->addDefinition($this->prefix('translator'))
+      ->setClass('Vojir\\BlankTranslator\\BlankTranslator');
+  }
+
+}
